b090dc2d6f17afb5340e0b4a6e9cab485000dafc
liuzhongpo <liuzhongpo@gaosiedu.com>
2019-03-21 18:41:48 +0800

    Spring Boot使用@Async实现异步调用：自定义线程池

---
+++ Chapter7-3/src/test/java/com/lzplzp/ApplicationTests.java
@@ -0,0 +1,27 @@
+package com.lzplzp;
+
+import com.lzplzp.task.Task;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = Chapter1Application.class)
+public class ApplicationTests {
+
+    @Autowired
+    private Task task;
+
+    @Test
+    public void test() throws Exception {
+
+        task.doTaskOne();
+        task.doTaskTwo();
+        task.doTaskThree();
+
+        Thread.currentThread().join();
+    }
+
+}
