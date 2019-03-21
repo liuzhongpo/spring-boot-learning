a67fcc3a5b6a311a85c330ca09ac0c827818089a
liuzhongpo <liuzhongpo@gaosiedu.com>
2019-03-21 14:32:42 +0800

    Spring Boot构建复杂RESTful API与单元测试

---
+++ Chapter3-1/src/main/java/com/lzplzp/web/UserController.java
@@ -0,0 +1,57 @@
+package com.lzplzp.web;
+
+import com.lzplzp.entity.User;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+/**
+ * @RestController 等价于  @Controller和@ResponseBody
+ */
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    static  Map<Integer,User> users = Collections.synchronizedMap(new HashMap<>());
+
+    @RequestMapping(value="/", method=RequestMethod.GET)
+    public List<User> getUserList() {
+        // 处理"/users/"的GET请求，用来获取用户列表
+        // 还可以通过@RequestParam从页面中传递参数来进行查询条件或者翻页信息的传递
+        List<User> r = new ArrayList<User>(users.values());
+        return r;
+    }
+
+    @RequestMapping(value="/", method=RequestMethod.POST)
+    public String postUser(@ModelAttribute User user) {
+        // 处理"/users/"的POST请求，用来创建User
+        // 除了@ModelAttribute绑定参数之外，还可以通过@RequestParam从页面中传递参数
+        users.put(user.getId(), user);
+        return "success";
+    }
+
+    @RequestMapping(value="/{id}", method=RequestMethod.GET)
+    public User getUser(@PathVariable Integer id) {
+        // 处理"/users/{id}"的GET请求，用来获取url中id值的User信息
+        // url中的id可通过@PathVariable绑定到函数的参数中
+        return users.get(id);
+    }
+
+    @RequestMapping(value="/{id}", method=RequestMethod.PUT)
+    public String putUser(@PathVariable Integer id, @ModelAttribute User user) {
+        // 处理"/users/{id}"的PUT请求，用来更新User信息
+        User u = users.get(id);
+        u.setName(user.getName());
+        u.setAge(user.getAge());
+        users.put(id, u);
+        return "success";
+    }
+
+    @RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+    public String deleteUser(@PathVariable Integer id) {
+        // 处理"/users/{id}"的DELETE请求，用来删除User
+        users.remove(id);
+        return "success";
+    }
+
+}
