abd376a479cbf001ddde13d7b6fae9ef6a5d505a
liuzhongpo <liuzhongpo@gaosiedu.com>
2019-03-21 17:42:24 +0800

    Spring Boot使用MyBatis访问数据库

---
+++ Chapter5-3/src/test/java/com/lzplzp/ApplicationTests.java
@@ -0,0 +1,28 @@
+package com.lzplzp;
+
+import com.lzplzp.entity.User;
+import com.lzplzp.mapper.UserMapper;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = Chapter1Application.class)
+public class ApplicationTests {
+
+	@Autowired
+	private UserMapper userMapper;
+
+	@Test
+	@Rollback
+	public void findByName() throws Exception {
+		userMapper.insert("AAA", 20);
+		User u = userMapper.findByName("AAA");
+		Assert.assertEquals(20, u.getAge().intValue());
+	}
+
+}
